b0c19966256e20b3bd410b00a449d0e91ef22e83
tirthkparikh <tirthkparikh@gmail.com>
2024-10-08 01:58:53 +0530

    babySteps lec-2

---
+++ Beginner/BabySteps/Lec2/Main.java
@@ -0,0 +1,37 @@
+package Beginner.BabySteps.Lec2;
+
+public class Main {
+    public static void main(String args[]){
+
+        // Integer    int...to store numeric
+        // String     to store word
+        // float      10.3
+        // double     10.20
+        // boolean    true/false
+        // char       A/B/@/$
+
+        int age = 25;
+        String name = "Tirth";
+        float cgpa = 9.5f;
+        double num =10.3;
+        boolean married = false;
+        char ch = 'W';
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(cgpa);
+        System.out.println(num);
+        System.out.println(married);
+        System.out.println(ch);
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a-b);
+        System.out.println(b/a);
+        System.out.println(b%a);
+        
+
+    }
+}
