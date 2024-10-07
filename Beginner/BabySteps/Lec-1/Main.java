9f4dd498238e636823589c38a47fe5ad3526ce20
tirthkparikh <tirthkparikh@gmail.com>
2024-10-08 01:36:58 +0530

    BabySteps Lec-1

---
+++ Beginner/BabySteps/Lec-1/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String [] args){
+
+        // Print() will print them on same line
+        // System.out.print("HelloWorld");
+        // System.out.print("HelloWorld");
+        // System.out.print("HelloWorld");
+        // System.out.print("HelloWorld");
+        // System.out.print("HelloWorld");
+
+        // Println will print them on new line
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+    }
+}
